5d136403b16532e5ef9a957b55724d02006b17ea
Marco Salles <marco.salles@caelum.com.br>
2018-01-02 15:06:38 -0200

    adding post route with body-parsing

--- app/controllers/PaymentController.js
+++ app/controllers/PaymentController.js
@@ -1,9 +1,21 @@
-const PaymentController = function (app) {
+const PaymentController = (function (app) {
+	const routes = {
+		list: '/payments',
+		create: '/payments/create'
+	};
 
-	app.get('/payments', function (req, res) {
-		res.send('200 OK');
+	app.get(routes.list, function (req, res) {
+		res.send(routes.list + ' 200 OK');
 	});
-};
+
+	app.post(routes.create, function (req, res) {
+		let payment = req.body;
+		console.log('payment', payment);
+		res.send(routes.create + ' 200 OK');
+	});
+
+	return routes;
+});
 
 module.exports = function (app) {
 	return new PaymentController(app);
